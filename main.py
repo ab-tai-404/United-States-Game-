8a9d16254091d5bcd3100b42ae5dd4f6319b7d35
Abdeladim <tailaba999@gmail.com>
2025-07-21 21:42:36 +0100

    developed my project

--- main.py
+++ main.py
@@ -2,32 +2,38 @@ from turtle import Turtle ,Screen
 import pandas as pd
 
 screen = Screen()
-screen.bgpic("blank_states_img.gif")
+image = "blank_states_img.gif"
+screen.bgpic(image)
+screen.title("U.S. States Game")
 
 data = pd.read_csv("50_states.csv")
-states  = []
-
-for name_states in data.state :
-    states.append(name_states)
+all_states  = data.state.to_list()
 
 
 timy = Turtle()
 timy.penup()
 timy.ht()
+correct_answer = []
+is_on = 50
 
-is_on = True
-print(states)
-while is_on :
-    answer= screen.textinput("NIM", "Name of first player:")
-
-    if answer in states :
-        x = int(data[data.state == answer].x.iloc[0])
-        y = int(data[data.state == answer].y.iloc[0])
+while len(correct_answer) < 50 :
+    print(all_states)
+    answer_state = screen.textinput(f"{len(correct_answer)}/50 States Correct",
+                                    "Name of first player:").title()
+    answer= answer_state.lower()
+    print(answer)
+    if answer_state in all_states :
+        state_data = data[data.state == answer_state]
+        x = int(state_data.x.iloc[0])
+        y = int(state_data.y.iloc[0])
         timy.goto(x, y)
-        timy.write(f"{answer}", align='left', font=('Arial', 8, 'normal'))
+        timy.write(f"{answer_state}", align='left', font=('Arial', 9 , 'normal'))
+        correct_answer.append(answer_state)
+    elif answer_state == "Exit" :
+        state_not_know = [item for item in all_states if item not in correct_answer]
+        df = pd.DataFrame(state_not_know)
+        df.to_csv("states_to_learn.csv")
+        break
 
-    elif answer == "off" :
-        is_on = False
 
 screen.exitonclick()
-
---
+++ main1.py
@@ -0,0 +1,3 @@
+e = [3,23,15,6]
+w = [3,56,6,6]
+print([item for item in w if item not in  e])
